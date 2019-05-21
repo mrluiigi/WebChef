bce326f729de075c873c8a81a9132456a252725b
PADBarbosa <36645154+PADBarbosa@users.noreply.github.com>
2019-05-21 15:21:08 +0100

    Script povoamento atualizado

--- Base de Dados/Query.sql
+++ Base de Dados/Query.sql
@@ -4,19 +4,28 @@ INSERT INTO Receita (id_receita, nome, descricao, informacao_nutricional, duraca
 	VALUES 
 		(1, 'Bifinhos', 'com cogumelos', '50Kcal', '00:20:00.0000000', 'https://youtu.be/gS2WtZHvvWk?t=', '~/Images/sopaAgriao.jpg', 2, 'facil', 'carne'),
 		(2, 'Massa', 'bolonhesa', '60Kcal', '00:40:00.0000000', 'www.ajuda.com', 'massa.jpg', 4, 'média', 'carne');
+SELECT * FROM  Receita;
+DELETE FROM Receita;
+
 
 
 Insert into Acao (id_acao, nome, descricao)
 	VALUES
 		(1, 'cozer', 'meter na agua quente'),
 		(2, 'fritar', 'meter em oleo quente');
+SELECT * FROM Acao;
+DELETE FROM Acao;
+
 
 
 Insert into Passo (id_passo, descricao, timestamp, id_acao, duracao)
 	VALUES
 		(1, 'Meter ovos a cozer', '0073', 1, 300),
 		(2, 'Fritas os bifes', '0010', 2, 450),
-		(3, 'Fritar os cogumelos', '0017', 2, NULL);
+		(3, 'Fritar os cogumelos', '0017', 2, null);
+SELECT * FROM Passo;
+DELETE FROM Passo;
+
 
 
 Insert into ReceitaPasso (id_passo, id_receita, numero)
@@ -24,6 +33,9 @@ Insert into ReceitaPasso (id_passo, id_receita, numero)
 		(2, 1, 1),
 		(1, 1, 2),
 		(3, 1, 3);
+SELECT * FROM ReceitaPasso;
+DELETE FROM ReceitaPasso;
+
 
 
 Insert into Ingrediente (id_ingrediente, designacao, imagem)
@@ -31,6 +43,9 @@ Insert into Ingrediente (id_ingrediente, designacao, imagem)
 		(1, 'cogumelo', '~/Images/cogumelos.jpg'),
 		(2, 'bife', '~/Images/bife.jpg'),
 		(3, 'óleo', '~/Images/oleo.jpg');
+SELECT * FROM Ingrediente;
+DELETE FROM Ingrediente;
+
 
 
 Insert into PassoIngrediente (id_passo, id_ingrediente, quantidade)
@@ -38,28 +53,11 @@ Insert into PassoIngrediente (id_passo, id_ingrediente, quantidade)
 		(3, 1, 16),
 		(2, 2, 2),
 		(2, 3, 1);
+SELECT * FROM PassoIngrediente;
+DELETE FROM PassoIngrediente;
 
 
 
 
 
-
-
-SELECT * FROM Utilizador;
-
-Delete from Passo;
-
-
-SELECT * FROM Receita;
-
-Delete from Receita;
-
-
-
-
-
-select * from Passo;
-
-delete from ReceitaPasso;
-delete from Passo;
 
